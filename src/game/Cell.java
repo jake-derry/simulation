ff820e8441d2f32c073618277db87cdc5dae8e61
Jake Derry <jad111@duke.edu>
2019-09-16 18:02:32 -0400

    Made JavaDocs of Simulation and Cell more detailed and fixed method signatures

--- src/game/Cell.java
+++ src/game/Cell.java
@@ -3,38 +3,57 @@ package game;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+/**
+ * This class represents a single cell in a cellular
+ * automata simulation that is stored in a grid. It
+ * holds information the cells current and next state
+ * to prevent incorrect updating of cells.
+ *
+ * The cell's state cannot be changed directly. Instead,
+ * the state is updated as the next state on a method call.
+ *
+ * @author Jake Derry
+ */
 public class Cell {
     private int myState;
     private int myNextState;
     private Rectangle myRectangle;
 
     /**
-     * Setter for myNextState
-     * @param next  new value of myNextState
+     * Sets the next state to the parameter next. To change
+     * the state, the setNextState method should be called
+     * and then the {link #stepState(Color[] palette) stepState}
+     * method which sets the state to the next state.
+     *
+     * @param next  sets the next state
      */
-    void setNextState(int next) {
+    public void setNextState(int next) {
 
     }
 
     /**
-     * Sets myState to the value of
-     * myNextState and sets the color
-     * of the cell rectangle to the
-     * palette color that matches the
-     * state
+     * Sets the current state to the next state, stepping the state
+     * forward. Once, the state has been updated, the method sets
+     * the color of the rectangle to match the palette taken in as
+     * a parameter. This palette is expressed as an array of colors
+     * where the state i's color is at index i.
+     *
      * @param palette   array of colors of the same length
      *                  as the number of states for a
      *                  simulation
      */
-    void stepState(Color[] palette) {
+    public void stepState(Color[] palette) {
 
     }
 
     /**
-     * Getter for myRectangle
-     * @return      myRectangle
+     * Gets the cell's rectangle object. The Visualization class will
+     * leverage this method to retrieve the cell's objects and display
+     * them.
+     *
+     * @return      the cell's rectangle
      */
-    Rectangle getRectangle() {
+    public Rectangle getRectangle() {
         return null;
     }
 
--- src/game/Simulation.java
+++ src/game/Simulation.java
@@ -1,34 +1,48 @@
 package game;
 
+/**
+ * This abstract class runs a simulation of any
+ * type. This type changes how the simulation is
+ * updated. The simulation contains an array of cells
+ * that are the basis of the cellular automata simulation.
+ *
+ * This simulation constantly updates if it is running.
+ *
+ * @author Jake Derry
+ */
 abstract public class Simulation {
     private Cell[][] grid;
     private boolean running;
 
-    /**
-     * Steps the simulation if the
-     * running variable is true
-     */
-    abstract public void update();
+    Simulation() { }
 
     /**
-     * Sets the running variable
-     * to false
+     * Sets the next state of all cells in the grid
+     * implemented based on type of simulation. Only
+     * does this when the running variable is set to true.
+     */
+    public abstract void update();
+
+    /**
+     * Sets the running variable to false which stops
+     * the {link #update update} method from running.
      */
     public void stop() {
 
     }
 
     /**
-     * Sets the running variable
-     * to true
+     * Sets the running variable to true which resumes
+     * the {link #update update} method to run.
      */
     public void start() {
 
     }
 
     /**
-     * Getter for grid
-     * @return  grid
+     * Gets the grid of Cells of the Simulation.
+     *
+     * @return  grid of Cells
      */
     public Cell[][] getGrid() {
         return null;
