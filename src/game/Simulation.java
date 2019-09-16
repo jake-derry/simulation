c62247751df834b5c51e63eac6c9b1b069959ec2
Jake Derry <jad111@duke.edu>
2019-09-16 17:02:14 -0400

    Generated empty methods for Simulation

--- src/game/Simulation.java
+++ src/game/Simulation.java
@@ -1,1 +1,38 @@
 package game;
+
+public class Simulation {
+    private Cell[][] grid;
+    private boolean running;
+
+    /**
+     * Steps the simulation if the
+     * running variable is true
+     */
+    public void update() {
+
+    }
+
+    /**
+     * Sets the running variable
+     * to false
+     */
+    public void stop() {
+
+    }
+
+    /**
+     * Sets the running variable
+     * to true
+     */
+    public void start() {
+
+    }
+
+    /**
+     * Getter for grid
+     * @return  grid
+     */
+    public Cell[][] getGrid() {
+        return null;
+    }
+}
