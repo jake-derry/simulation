53345b8e885256f6b33c6efbe602b4bd5dd6f261
Jake Derry <jad111@duke.edu>
2019-09-16 17:18:04 -0400

    Generated empty methods in Cell

--- src/game/Cell.java
+++ src/game/Cell.java
@@ -1,1 +1,41 @@
 package game;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Cell {
+    private int myState;
+    private int myNextState;
+    private Rectangle myRectangle;
+
+    /**
+     * Setter for myNextState
+     * @param next  new value of myNextState
+     */
+    void setNextState(int next) {
+
+    }
+
+    /**
+     * Sets myState to the value of
+     * myNextState and sets the color
+     * of the cell rectangle to the
+     * palette color that matches the
+     * state
+     * @param palette   array of colors of the same length
+     *                  as the number of states for a
+     *                  simulation
+     */
+    void stepState(Color[] palette) {
+
+    }
+
+    /**
+     * Getter for myRectangle
+     * @return      myRectangle
+     */
+    Rectangle getRectangle() {
+        return null;
+    }
+
+}
