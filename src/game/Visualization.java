8141e610d4220de4c6a7040ff9d69b513ec86795
Matt Harris <matthew.harris1@duke.edu>
2019-09-16 17:39:34 -0400

    Added visualization skeleton.

--- src/game/Visualization.java
+++ src/game/Visualization.java
@@ -1,1 +1,33 @@
 package game;
+
+import javafx.scene.Group;
+import javafx.scene.Node;
+
+/**
+ * Visualization section of our CAApp.
+ *
+ * @author Matt Harris
+ */
+public class Visualization{
+
+    /**
+     * Constructor for Visualization
+     */
+    public Visualization(){
+    }
+
+    /**
+     * setUp the group to be displayed in CAApp
+     */
+    public Group setUp(Node root){
+        Group group = new Group();
+        return group;
+    }
+
+    /**
+     * visualize: step through cells and update colors accordingly
+     */
+    public void visualize(){
+
+    }
+}
