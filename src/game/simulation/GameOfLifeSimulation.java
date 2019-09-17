e92259b3a7205af4a0bf40309d0d996895645cc4
Jake Derry <jad111@duke.edu>
2019-09-17 17:05:38 -0400

    Added description of GameOfLife

--- src/game/simulation/GameOfLifeSimulation.java
+++ src/game/simulation/GameOfLifeSimulation.java
@@ -1,6 +1,11 @@
 package game.simulation;
 
+/**
+ * This simulation runs Conway's Game of Life which
+ * has rules that simulate life.
+ */
 public class GameOfLifeSimulation extends Simulation {
+
     @Override
     public void update() {
     }
