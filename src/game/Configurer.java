8381d40489e8f0d8a7c6f0decc424935f21750e7
Jonah Knapp <jonah.knapp@duke.edu>
2019-09-18 19:37:52 -0400

    Configurer reads the file and creates the cellular array for Simulation

--- src/game/Configurer.java
+++ src/game/Configurer.java
@@ -19,33 +19,33 @@ import org.w3c.dom.NodeList;
  */
 public class Configurer{
     //Tags used within XML file
-    private static final String COLUMN_TAG = "col";
-    private static final String ROW_TAG = "row";
-    private static final String STATE_TAG = "state";
-    /**
-     *Basic functionality of reading XML based on online example
+    private static final String COLUMN_TAG = "cols";
+    private static final String ROW_TAG = "rows";
+    private static final String CELL_COLUMN_TAG = "col";
+    private static final String CELL_ROW_TAG = "row";
+    private static final String CELL_STATE_TAG = "state";
+
+    /**Reads XML file. Creates a document using the DocumentBuilder class. Uses this information to create a
+     * cellular array and ultimately passes this information to the Simulation class.
+     *
      * @param myFile XML to be read
      * @return Simulation created based on XML file
      */
-    public static Simulation readFile(String myFile){
+    public static Simulation getSimulation(String myFile){
+        Document simDoc = readFile(myFile);
+        Cell[][] cellArray = initializeCellArray(Integer.parseInt(getFirstElement((Element) simDoc, ROW_TAG)),
+                Integer.parseInt(getFirstElement((Element) simDoc, COLUMN_TAG)));
+        updateActiveCells(simDoc, cellArray);
+        return null;
+    }
+
+    private static Document readFile(String myFile){
         try{
             File simFile = new File("data/" + myFile);
-            DocumentBuilder simDocumentBuilder = createDocumentBuilder();
+            DocumentBuilder simDocumentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
             Document simDoc = simDocumentBuilder.parse(simFile);
-            System.out.println("Root element :" + simDoc.getDocumentElement().getNodeName());
-            System.out.println("Root element :" + simDoc.getElementsByTagName("author")
-                    .item(0).getTextContent());
-            NodeList nList = simDoc.getElementsByTagName("cell");
-            for(int i = 0 ; i < nList.getLength(); i++){
-                Element myCell = (Element) nList.item(i);
-                System.out.println("Cell" + i);
-                System.out.println("Row :" + myCell.getElementsByTagName(ROW_TAG)
-                        .item(0).getTextContent());
-                System.out.println("Col :" + myCell.getElementsByTagName(COLUMN_TAG)
-                        .item(0).getTextContent());
-                System.out.println("State :" + myCell.getElementsByTagName(STATE_TAG)
-                        .item(0).getTextContent());
-            }
+            printActiveCells(simDoc);
+            return simDoc;
         } catch (Exception e) {
             e.printStackTrace();
             errorAlert(e.getMessage());
@@ -54,34 +54,48 @@ public class Configurer{
         return null;
     }
 
-    private static DocumentBuilder createDocumentBuilder(){
-        try {
-            return DocumentBuilderFactory.newInstance().newDocumentBuilder();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-            errorAlert(e.getMessage());
-            return null;
+    private static Cell[][] initializeCellArray(int Rows, int Cols){
+        Cell[][] myCellArray = new Cell[Rows][Cols];
+        for(int i = 0; i<Rows; i++){
+            for(int j = 0; j<Cols; j++){
+                myCellArray[i][j].setState(0);
+            }
+        }
+        return myCellArray;
+    }
+
+    private static void updateActiveCells(Document myDoc, Cell[][] myCells){
+        NodeList nList = myDoc.getElementsByTagName("cell");
+        for(int i = 0 ; i < nList.getLength(); i++){
+            Element myCell = (Element) nList.item(i);
+            int myRow = Integer.parseInt(getFirstElement(myCell, CELL_ROW_TAG);
+            int myCol = Integer.parseInt(getFirstElement(myCell, CELL_COLUMN_TAG);
+            myCells[myRow][myCol].setState(Integer.parseInt(getFirstElement(myCell, CELL_STATE_TAG)));
         }
     }
+
+    private static void printActiveCells(Document myDoc) {
+        NodeList nList = myDoc.getElementsByTagName("cell");
+        for(int i = 0 ; i < nList.getLength(); i++){
+            Element myCell = (Element) nList.item(i);
+            System.out.println("Cell " + i + ":");
+            System.out.println(CELL_ROW_TAG + " " + getFirstElement(myCell, CELL_ROW_TAG));
+            System.out.println(CELL_COLUMN_TAG + " " + getFirstElement(myCell, CELL_COLUMN_TAG));
+            System.out.println(CELL_STATE_TAG + " " + getFirstElement(myCell, CELL_STATE_TAG));
+        }
+    }
+
+
+    private static String getFirstElement(Element myElement, String TagName){
+        return myElement.getElementsByTagName(TagName).item(0).getTextContent();
+        //System.out.println("Root element :" + simDoc.getDocumentElement().getNodeName());
+        //System.out.println("Author :" + simDoc.getElementsByTagName("author")
+        //       .item(0).getTextContent());
+    }
+
 
     private static void errorAlert(String myErrorMessage){
         new Alert(AlertType.ERROR, myErrorMessage).showAndWait();
     }
-
-    /**
-     *
-     * @param myDoc Document to extract element from
-     * @param tag Tag to search for within the document
-     * @return single element that corresponds to a tag
-     */
-    private static String getElement(Document myDoc, String tag){
-        return myDoc.getElementsByTagName("author").item(0).getTextContent();
-    }
-
-
-
-
-    //Config will eventually: create array of cells with state initialized. This will be passed into Simulation
-
 
 }
