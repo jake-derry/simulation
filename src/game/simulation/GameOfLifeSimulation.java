37316a7fa7264f1299a14e61ba23a5e236f78417
Jake Derry <jad111@duke.edu>
2019-09-17 22:41:24 -0400

    Updateimplemented for Game of Life

--- src/game/simulation/GameOfLifeSimulation.java
+++ src/game/simulation/GameOfLifeSimulation.java
@@ -1,7 +1,5 @@
 package game.simulation;
 
-import game.Cell;
-
 
 /**
  * This simulation runs Conway's Game of Life which
@@ -15,21 +13,52 @@ public class GameOfLifeSimulation extends Simulation {
     public void update() {
         for (int i = 0; i < getGridRowCount(); i++) {
             for (int j = 0; j < getGridColumnCount(); i++) {
-                updateCell(i, j);
+                getCell(i, j).setNextState(nextState(i, j));
             }
         }
     }
 
-    private void updateCell(int i, int j) {
+    private int nextState(int i, int j) {
         int[] neighborStates = getNeighborStates(i, j);
+        int count = countLiveNeighbors(neighborStates);
+        int state = getCell(i, j).getState();
+        if (state == DEAD) {
+            if (count == 3) {
+                return LIVE;
+            }
+            else {
+                return DEAD;
+            }
+        }
+
+        else {
+            if (count < 2 || count > 3) {
+                return DEAD;
+            }
+            else {
+                return LIVE;
+            }
+        }
+    }
+
+    private int countLiveNeighbors(int[] neighborStates) {
+        int count = 0;
+        for (int i = 0; i < neighborStates.length; i++) {
+            if (neighborStates[i] == LIVE) count++;
+        }
+        return count;
     }
 
     private int[] getNeighborStates(int i, int j) {
-        int[] neighborStates = new int[4];
-        neighborStates[0] = getCell(i-1, j).getState();
-        neighborStates[1] = getCell(i+1, j).getState();
-        neighborStates[2] = getCell(i, j-1).getState();
-        neighborStates[3] = getCell(i, j+1).getState();
+        int[] neighborStates = new int[8];
+        neighborStates[0] = getCell(i - 1, j).getState();
+        neighborStates[1] = getCell(i + 1, j).getState();
+        neighborStates[2] = getCell(i - 1, j - 1).getState();
+        neighborStates[3] = getCell(i + 1, j - 1).getState();
+        neighborStates[4] = getCell(i, j - 1).getState();
+        neighborStates[5] = getCell(i - 1, j + 1).getState();
+        neighborStates[6] = getCell(i + 1, j + 1).getState();
+        neighborStates[7] = getCell(i, j + 1).getState();
         return neighborStates;
     }
 }
