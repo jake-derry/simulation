53493c50b279fa7d63c3fbf8bfe80281b20589f6
Jake Derry <jad111@duke.edu>
2019-09-24 22:28:58 -0400

    Created the CellGrid class

---
+++ src/game/Simulation/CellGrid.java
@@ -0,0 +1,16 @@
+package game.Simulation;
+
+import game.Simulation.Cell.Cell;
+
+import java.util.Iterator;
+
+public class CellGrid implements Iterable<Cell> {
+    private Cell[][] myCellGrid;
+
+    public CellGrid() {}
+
+    @Override
+    public Iterator<Cell> iterator() {
+        return null;
+    }
+}
