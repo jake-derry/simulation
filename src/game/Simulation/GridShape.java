0d7ec8def30d2bed9aceb499c3aee5e33b42d66f
Jake Derry <jad111@duke.edu>
2019-09-24 23:21:06 -0400

    Stored GridShape shape name in xml file

--- src/game/Simulation/GridShape.java
+++ src/game/Simulation/GridShape.java
@@ -4,13 +4,13 @@ import java.util.AbstractMap;
 import java.util.List;
 
 public enum GridShape {
-    RECTANGLE("rectangle.xml", "Rectangle"),
-    TRIANGLE("triangle.xml", "Triangle"),
-    HEXAGON("hexagon.xml", "Hexagon"),
+    RECTANGLE("rectangle.xml"),
+    TRIANGLE("triangle.xml"),
+    HEXAGON("hexagon.xml"),
     ;
 
     private List<AbstractMap.SimpleEntry<Integer, Integer>> myDisplacementMap;
     private String myShape;
 
-    GridShape(String filename, String shape) { }
+    GridShape(String filename) { }
 }
