508616ffc94c1250bc69219d41c019f6f3d5d6ab
Jake Derry <jad111@duke.edu>
2019-09-26 15:02:24 -0400

    Implemetned FireCell

--- src/game/Simulation/Cell/FireCell.java
+++ src/game/Simulation/Cell/FireCell.java
@@ -2,31 +2,52 @@ package game.Simulation.Cell;
 
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Random;
 
 public class FireCell extends Cell {
     private static final int EMPTY = 0;
     private static final int BURNING = 1;
     private static final int TREE = 2;
 
-    public FireCell(int state, Iterator<Cell> neighborIterator) {
+    private double myProbCatch;
+    private Random random;
+    private Map<Integer, Integer> countMap;
+
+    public FireCell(int state, Iterator<Cell> neighborIterator, double probCatch) {
         super(state, neighborIterator);
+        myProbCatch = probCatch;
+        random = new Random();
+        updateCountMap();
     }
 
     @Override
     public void updateNext() {
-        Map<Integer, Integer> countMap = CellUtils.countMap(getNeighbors());
-
+        updateCountMap();
+        int nextState;
         switch (getState()) {
-            case (EMPTY) : {
-                
-            }
-            case (BURNING) : {
-
+            case (EMPTY) :
+            case (BURNING) :{
+                nextState = EMPTY;
             }
             case (TREE) : {
+                nextState = (burning() ? BURNING : TREE);
 
             }
         }
 
+
+    }
+
+    private void updateCountMap() {
+        countMap = CellUtils.countMap(getNeighbors());
+    }
+
+    private boolean burning() {
+        for (int i = 0; i < countMap.get(EMPTY); i++) {
+            if (random.nextDouble() <= myProbCatch) {
+                return true;
+            }
+        }
+        return false;
     }
 }
