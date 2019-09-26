7c22b795013eab884ce85ae71a087604280fec4d
Jake Derry <jad111@duke.edu>
2019-09-26 17:13:19 -0400

    Enumerated states

---
+++ src/game/Simulation/State.java
@@ -0,0 +1,15 @@
+package game.Simulation;
+
+public enum State {
+    EMPTY,
+    LIVE,
+    BEYOND_EDGE,
+    WALL,
+    WATER,
+    PREDATOR,
+    PREY,
+    GROUP_A,
+    GROUP_B,
+    BURNING,
+    TREE,
+}
