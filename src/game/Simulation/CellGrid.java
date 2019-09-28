a900617575f3a2796fb870c5e5cd45fb1a703f74
Jake Derry <jad111@duke.edu>
2019-09-28 18:53:51 -0400

    Implemented the Cell Iterator

--- src/game/Simulation/CellGrid.java
+++ src/game/Simulation/CellGrid.java
@@ -3,16 +3,16 @@ package game.Simulation;
 import game.Simulation.Cell.Cell;
 
 import java.util.Iterator;
+import java.util.Map;
 
 public class CellGrid implements Iterable<Cell> {
     private Cell[][] myCellGrid;
 
-    public CellGrid(Cell[][] cellGrid) {
-        myCellGrid = cellGrid;
+    public CellGrid(Map<String, Object> parameterMap, int[][] cellGrid) {
     }
 
     @Override
     public Iterator<Cell> iterator() {
-        return null;
+        return new CellIterator(myCellGrid);
     }
 }
